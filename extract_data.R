a353430e9d6ee68e921a95d39b07210b330cfe66
catherineperkins <catherine.perkins@phs.scot>
2022-11-29 17:24:59 +0000

    Set up extraction from open data

---
+++ extract_data.R
@@ -0,0 +1,95 @@
+###############################################.
+## ScotPHO - Life expectancy - Council Area ----
+###############################################.
+
+# Firstly, update the data in the 'HLE ScotPHO Web Section' folder (see below) - the
+# data is extracted from the SG opendata platform.
+# Then run this code to save the updated data file into analyst's folder for running the shiny app
+
+# data queried directly from statistics.gov
+# install the opendata scotland r package which communicates with the statistics.gov wesbite api
+# install.packages("devtools")
+
+devtools::install_github("datasciencescotland/opendatascot")
+
+library(opendatascot) # to extract from statistics.gov
+library(phsmethods)   # to add location names
+library(readr)        # to write csv
+library(dplyr)
+# datasets <- ods_all_datasets() # to see available datasets on statistic.gov.scot
+  
+# UPDATE the analyst's folder - where data should be saved for shiny app to run
+shiny_folder <- "/PHI_conf/ScotPHO/1.Analysts_space/Catherine/scotpho-life-expectancy-ca/shiny_app/data/"
+
+# updated data file location
+data_folder <- "/PHI_conf/ScotPHO/Life Expectancy/HLE ScotPHO Web Section/Scotland/"
+
+
+
+# parameters used to filter the opendata
+simd <- c("all")
+urban_rural <- c("all")
+age_select <- "0-years"
+
+
+###############################################.
+# Healthy life expectancy data by CA
+###############################################.
+
+ods_structure("healthy-life-expectancy") # see structure and variables of this dataset
+
+# date range for HLE
+date_range_hle <- c("2015-2017", "2016-2018", "2017-2019", "2018-2020") # add most recent year
+
+# extract data
+hle = ods_dataset("healthy-life-expectancy", refPeriod = date_range_hle, geography = "la",
+                  urbanRuralClassification = urban_rural,
+                  simdQuintiles = simd, measureType = "count") %>%
+  setNames(tolower(names(.))) %>%
+  rename("council" = refarea, "year" = refperiod) %>% 
+  filter(age == age_select) %>% 
+  mutate(measure = "Healthy life expectancy") %>% 
+  select(c("council", "year", "measure", "value", "sex")) %>% 
+  mutate(council = match_area(council)) %>% 
+  arrange(year, council, sex) %>% 
+  mutate(sex = case_when(sex == "male" ~ "Male",
+                         sex == "female" ~ "Female"))
+
+
+###############################################.
+# Life expectancy data by CA
+###############################################.
+
+ods_structure("Life-Expectancy") # see structure and variables of this dataset
+
+# date range for LE
+date_range_le <- c("2001-2003", "2002-2004", "2003-2005", "2004-2006", "2005-2007",
+                   "2006-2008", "2007-2009", "2008-2010", "2009-2011", "2010-2012",
+                   "2011-2013", "2012-2014", "2013-2015", "2014-2016", "2015-2017", 
+                   "2016-2018", "2017-2019", "2018-2020", "2019-2021") # add most recent year
+
+# extract data
+le = ods_dataset("Life-Expectancy", refPeriod = date_range_le, geography= "la",
+                  urbanRuralClassification = urban_rural,
+                  simdQuintiles = simd, measureType = "count") %>%
+  setNames(tolower(names(.))) %>%
+  rename("council" = refarea, "year" = refperiod) %>% 
+  filter(age == age_select) %>% 
+  mutate(measure = "Life expectancy") %>% 
+  select(c("council", "year", "measure", "value", "sex")) %>% 
+  mutate(council = match_area(council)) %>% 
+  arrange(year, council, sex) %>% 
+  mutate(sex = case_when(sex == "male" ~ "Male",
+            sex == "female" ~ "Female"))
+
+
+# combine datasets
+le_hle <- rbind(le, hle)
+
+# save as csv
+write_csv(le_hle, paste0(data_folder, "le_hle_ca.csv"))
+
+# Save data to shiny_app folder
+saveRDS(le_hle, file = paste0(shiny_folder,"le_hle_ca.rds"))
+
+# END
---
+++ shiny_app/app.R
@@ -0,0 +1,134 @@
+###############################################.
+## ScotPHO - Life expectancy - NHS Board ----
+###############################################.
+
+# Code to create shiny chart of trends in life expectancy and healthy life expectancy by NHS Board 
+# This is published in the following section of the ScotPHO website: 
+# Population Dynamics > Deaths and life expectancy > Data > NHS Boards
+
+############################.
+## Global ----
+############################.
+############################.
+##Packages 
+
+library(dplyr) #data manipulation
+library(plotly) #charts
+library(shiny) #shiny apps
+
+# Data file
+ca_trend <- readRDS("data/le_hle_ca.rds")
+
+# Use for selection of areas
+council_list <- sort(unique(ca_trend$council))
+
+############################.
+## Visual interface ----
+############################.
+#Height and widths as percentages to allow responsiveness
+ui <- fluidPage(style="width: 650px; height: 500px; ",
+                div(style= "width:100%",
+                    h4("Chart 2. Life expectancy and healthy life expectancy at birth by Council Area"),
+                    div(style = "width: 50%; float: left;",
+                        selectInput("measure", label = "Select a measure type",
+                                    choices = c("Life expectancy", "Healthy life expectancy"), 
+                                    selected = "Life expectancy"))),
+                
+                div(style = "width: 50%; float: left;",
+                    selectInput("council", label = "Select Council Area", 
+                                choices = council_list,
+                                selected = "Scotland")),
+                
+                div(style= "width:100%; float: left;", #Main panel
+                    plotlyOutput("chart", width = "100%", height = "350px"),
+                    p("note: y-axis does not start at zero"),
+                    p(div(style = "width: 25%; float: left;", #Footer
+                          HTML("Source: <a href='https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/life-expectancy' target='_blank'>NRS</a>")),
+                      div(style = "width: 25%; float: left;",
+                          downloadLink('download_data', 'Download data')))
+                )
+) # fluidPage
+
+############################.
+## Server ----
+############################.
+server <- function(input, output) {
+  
+  output$chart <- renderPlotly({
+    
+    #Data for Council Area line
+    data_ca <- ca_trend %>% filter(council == input$council & measure == input$measure)
+     
+    
+    # Information to be displayed in tooltip
+    tooltip <- c(paste0(input$council, " - ", data_ca$sex, "<br>",
+                        "Time period (3 year average): ", data_ca$year, "<br>",
+                        input$measure, " (years): ", data_ca$value, "<br>"))
+    
+    # y-axis title
+    yaxistitle <- paste0(input$measure, " (years)")
+    
+    # Define line colours
+    pal <- c('#9B4393', '#1E7F84')
+    
+    # Define number of lines on chart
+    num <- length(unique(data_ca$sex))
+    
+    # set number of ticks depending on measure selected
+    if (input$measure == "Life expectancy") 
+      
+    {tick_freq <- 2}
+    
+    else {tick_freq <- 1}
+    
+      
+    # Buttons to remove from plot
+    bttn_remove <-  list('select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d',
+                         'autoScale2d',   'toggleSpikelines',  'hoverCompareCartesian',
+                         'hoverClosestCartesian', 'zoom2d', 'pan2d', 'resetScale2d')
+    
+    
+    plot <- plot_ly(data = data_ca, x=~year, y = ~value, 
+                    color= ~sex, colors = pal[1:num], 
+                    type = "scatter", mode = 'lines+markers', 
+                    symbol= ~sex, symbols = list('circle','square'), marker = list(size= 8),
+                    width = 650, height = 350,
+                    text=tooltip, hoverinfo="text")  %>%  
+
+      
+      # Layout
+      layout(annotations = list(), #It needs this because of a buggy behaviour
+             yaxis = list(title = yaxistitle, 
+                          #rangemode="tozero", 
+                          fixedrange=TRUE), 
+             xaxis = list(
+               title = list(text = "3 year average", standoff=20),
+               dtick = tick_freq,
+               fixedrange=TRUE#, 
+               #tickangle = 0
+             ),
+             font = list(family = 'Arial, sans-serif'), #font
+             margin = list(pad = 4, t = 50), #margin-paddings
+             hovermode = 'false',  # to get hover compare mode as default
+             legend = list(orientation = "h", x=0, y=1.2)) %>% 
+      config(displayModeBar= T, displaylogo = F, editable =F, modeBarButtonsToRemove = bttn_remove) 
+    # taking out plotly logo and collaborate button
+    
+  }) 
+  
+  
+  # Allow user to download data
+  output$download_data <- downloadHandler(
+    filename =  'le_and_hle_ca_data.csv', 
+    content = function(file) {
+      write.csv(ca_trend, file, row.names=FALSE) })
+  
+} # end of server
+
+############################.
+## Calling app ----
+############################.
+
+shinyApp(ui = ui, server = server)
+
+##END
